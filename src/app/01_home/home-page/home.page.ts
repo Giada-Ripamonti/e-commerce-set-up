78407dfadb0dde293684a412152ccf7a65401999
Giada-Ripamonti <ripamontigiada@gmail.com>
2022-09-29 18:05:07 +0200

    homepage

--- src/app/01_home/home-page/home.page.ts
+++ src/app/01_home/home-page/home.page.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss']
 })
 export class HomePage implements OnInit {
+  path: string = "assets/imgs/home-page-banner.jpg";
 
   constructor() { }
 
--- src/app/01_home/home.module.ts
+++ src/app/01_home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePage } from './home-page/home.page';
+import { CarouselComponent } from './carousel/carousel.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePage
+    HomePage,
+    CarouselComponent
   ],
   imports: [
     CommonModule
